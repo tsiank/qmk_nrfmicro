b011bf6ce9f49a16f0655efa33c78c6dd0d3e56e
joric <joric@users.noreply.github.com>
2020-05-14 05:47:29 +0500

    better text on display

--- keyboards/jorne_ble/lib/rgb_state_reader.c
+++ keyboards/jorne_ble/lib/rgb_state_reader.c
@@ -8,8 +8,8 @@ char rbf_info_str[24];
 const char *read_rgb_info(void) {
 
   if (rgblight_config.enable)
-    snprintf(rbf_info_str, sizeof(rbf_info_str), "RGB: %-2d HSV: %02x%02x%02x",
-      rgblight_config.mode, rgblight_config.hue, rgblight_config.sat, rgblight_config.val);
+    snprintf(rbf_info_str, sizeof(rbf_info_str), "HSV: %02x%02x%02x mode: %2d",
+      rgblight_config.hue, rgblight_config.sat, rgblight_config.val, rgblight_config.mode);
   else
     snprintf(rbf_info_str, sizeof(rbf_info_str), "RGB: off");
 
